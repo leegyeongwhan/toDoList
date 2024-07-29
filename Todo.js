5ae15b0ffe52b66416c22ba7be8e7e4695554633
gylee <leekhy02@naver.com>
2024-07-29 22:38:15 +0900

    feat : 완료 분

--- Todo.js
+++ Todo.js
@@ -1,11 +1,19 @@
 const todoInputElem = document.querySelector('.todo-inner');
 const todoListElem = document.querySelector('.todo-list');
-const completeAllBtnElem = document.querySelector('.complete-all-btn');
-
 const leftItemsElem = document.querySelector('.left-items')
+
+const getActiveTodos = () => {
+    return todos.filter(todo => todo.isCompleted === false);
+}
+
+const setLeftItems = () => {
+    const leftTodos = getActiveTodos()
+    leftItemsElem.innerHTML = `${leftTodos.length} items left`
+}
 
 let todos = []; //현재 할일 목록 저장
 let id = 0;
+
 
 const setTodos = (newTodos) => {
     todos = newTodos;
@@ -15,30 +23,25 @@ const getAllTodos = () => {
     return todos;
 }
 
-const setIsAllCompleted = (bool) => { isAllCompleted = bool};
-
-const getCompletedTodos = () => {
-    return todos.filter(todo => todo.isCompleted === true );
-}
-
-let isAllCompleted = ''; // 전체 todos 체크 여부
-
 
 const deleteTodo = (todoId) => {
-    console.log(todoId);
-    setTodos(getAllTodos().filter(todo => todo.id !== todoId));
-    print();
+    const newTodos = getAllTodos().filter(todo => todo.id !== todoId );
+    setTodos(newTodos);
+    setLeftItems()
+    print()
 }
 
 const completeTodo = (todoId) => {
-    setTodos(getAllTodos().map(todo =>
-        todo.id === todoId ? {...todo, isCompleted: !todo.isCompleted} : todo
-    ));
+    const newTodos = getAllTodos().map(todo => todo.id === todoId ? {...todo,  isCompleted: !todo.isCompleted} : todo )
+    setTodos(newTodos);
     print();
+    setLeftItems()
+    checkIsAllCompleted();
 }
 
 const updateTodo = (text, todoId) => {
-    const newTodos = getAllTodos().map(todo => todo.id === todoId ? ({...todo, content: text}) : todo);
+    const currentTodos = getAllTodos();
+    const newTodos = currentTodos.map(todo => todo.id === todoId ? ({...todo, content: text}) : todo);
     setTodos(newTodos);
     print();
 }
--- TodoEventListener.js
+++ TodoEventListener.js
@@ -2,43 +2,6 @@ const getCompletedTodos = () => {
     return todos.filter(todo => todo.isCompleted === true );
 }
 
-let isAllCompleted = ''; // 전체 todos 체크 여부
-
-const setIsAllCompleted = (bool) => { isAllCompleted = bool};
-
-const completeAll = () => {
-    completeAllBtnElem.classList.add('checked');
-    const newTodos = getAllTodos().map(todo => ({...todo, isCompleted: true }) )
-    setTodos(newTodos)
-}
-
-const incompleteAll = () => {
-    completeAllBtnElem.classList.remove('checked');
-    const newTodos =  getAllTodos().map(todo => ({...todo, isCompleted: false }) );
-    setTodos(newTodos)
-}
-
-// 전체 todos의 check 여부 (isCompleted)
-const checkIsAllCompleted = () => {
-    if(getAllTodos().length === getCompletedTodos().length ){
-        setIsAllCompleted(true);
-        completeAllBtnElem.classList.add('checked');
-    }else {
-        setIsAllCompleted(false);
-        completeAllBtnElem.classList.remove('checked');
-    }
-}
-
-const onClickCompleteAll = () => {
-    if(!getAllTodos().length) return; // todos배열의 길이가 0이면 return;
-
-    if(isAllCompleted) incompleteAll(); // isAllCompleted가 true이면 todos를 전체 미완료 처리
-    else completeAll(); // isAllCompleted가 false이면 todos를 전체 완료 처리
-    setIsAllCompleted(!isAllCompleted); // isAllCompleted 토글
-    print(); // 새로운 todos를 렌더링
-    setLeftItems()
-}
-
 const createDeleteButton = (todoId) => {
     const delBtnElem = document.createElement('button');
     delBtnElem.classList.add('delBtn');
@@ -112,13 +75,17 @@ const print = () => {
 
 //리스너등록
 const init = () => {
-    todoInputElem.addEventListener('keypress', (e) => {
-        if (e.key === 'Enter') {
-            insertTodo(e.target.value);
-            todoInputElem.value = '';
+    todoInputElem.addEventListener('keypress', (e) =>{
+        if( e.key === 'Enter' ){
+            insertTodo(e.target.value); todoInputElem.value ='';
         }
     })
+
+    completeAllBtnElem.addEventListener('click',  onClickCompleteAll)
+
+    setLeftItems()
 }
+
 
 
 init()
