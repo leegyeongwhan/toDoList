afa26f959dfdccb88d5dd77f1d7353188b8fa0de
gylee <leekhy02@naver.com>
2024-08-22 23:03:38 +0900

    feat :  끝말잇기 webpack 설정 추가

--- src/2.끝말잇기/webpack.config.js
+++ src/2.끝말잇기/webpack.config.js
@@ -1,1 +1,29 @@
-module.exports = {};
+const path = require("path");
+const entry = require("next/dist/server/typescript/rules/entry");
+
+module.exports = {
+    name: 'wordRelay-dev',
+    mode: 'development',
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    entry: {
+        app: ['./client'],
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            loader: 'babel-loader',
+            options: {
+                presets: ["@babel/preset-env","@babel/preset-react"],
+                plugins: ["@babel/plugin-proposal-class-properties"],
+            },
+        }],
+    },
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "app.js"
+    },
+};
+
